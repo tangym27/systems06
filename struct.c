b7a0bf5d07aa125d77aa8e7d12768e692882ce9b
GarveyZhou <gzhou2@stuy.edu>
2018-10-02 16:56:48 -0400

    added testcases for modify

--- struct.c
+++ struct.c
@@ -4,20 +4,21 @@
 #include <time.h>
 
 
-struct mta { char* train; int delay; }; // creates struct prototype
+struct mta {char* train; int delay; }; // creates struct prototype
 struct mta a;
 
-int values(struct mta x){
+void values(struct mta x){
   printf("The train name is the %s train!\n", x.train);
   printf("Your train, on a daily basis, has %d delays.\n", x.delay);
-  return 0;
 }
 
 struct mta randomize(){
   srand(time(NULL));
   struct mta new;
-  new.train = "p";
-  new.train= "l";
+  int key = rand() % 24;
+  char x[25] = "ABCDEFLMGJZLSNQRW1234567";
+  char c = x[key];
+  new.train = &c;
   new.delay = (rand()/10000000);
   values(new);
   return new;
@@ -31,10 +32,14 @@ int modify(struct mta x, char* ntrain, int ndelay){
 }
   
 int main(){
-  a.train = "n";
-  a.delay = 15;
-  values(a);
-  modify(a, "b", 86);
+  struct mta a;
+  printf("Testing Modify---------\n");
+  printf("Expected:\nThe train name is the B train \n Your train, on a daily basis, has 86 delays\n Results\n");
+  modify(a, "B", 86);
+  printf("Expected:\nThe train name is the D train \n Your train, on a daily basis, has 90 delays\n Results\n");
+  modify(a, "D",90);
+  printf("Expected:The train name is the C train \n Your train, on a daily basis, has 100000 delays\n Results\n");
+  modify(a, "C", 100000);
   randomize();
   return 0;
 }
