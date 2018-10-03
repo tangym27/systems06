255b09cea35eae86b7f32ced5d5a380ba6e314b6
GarveyZhou <gzhou2@stuy.edu>
2018-10-03 11:44:39 -0400

    fixed issues

--- struct.c
+++ struct.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <time.h>
 
 
-struct mta {char* train; int delay; }; // creates struct prototype
+struct mta {char train[20]; int delay; }; // creates struct prototype
 struct mta a;
 
 void values(struct mta x){
-  printf("The train name is the %s train!\n", x.train);
+  printf("The train name is the: %s!\n", x.train);
   printf("Your train, on a daily basis, has %d delays.\n", x.delay);
 }
 
 struct mta randomize(){
   srand(time(NULL));
   struct mta new;
-  int key = rand() % 24;
-  char x[25] = "ABCDEFLMGJZLSNQRW1234567";
-  char c = x[key];
-  new.train = &c;
-  new.delay = (rand()/10000000);
+  int key = rand() % 23;
+  printf("%d\n",rand());
+  char x[] = "ABCDEFLMGJZLSNQRW1234567";
+  new.train[0] = x[key];
+  new.train[1] = 0;
+  char s[10] = " train";
+  strcat(new.train,s);
+  new.delay = (rand() % 100);
   values(new);
   return new;
 }
 
 int modify(struct mta x, char* ntrain, int ndelay){
-  x.train = ntrain;
+  char s[10] = " train";
+  x.train[0] = ntrain[0];
+  x.train[1] = 0;
+   strcat(x.train,s);
   x.delay = ndelay;
   values(x);
   return 0;
@@ -33,13 +39,20 @@ int modify(struct mta x, char* ntrain, int ndelay){
   
 int main(){
   struct mta a;
+  char s1[2] = "B";
+  char s2[2] = "D";
+  char s3[2] = "C";
+  
   printf("Testing Modify---------\n");
-  printf("Expected:\nThe train name is the B train \n Your train, on a daily basis, has 86 delays\n Results\n");
-  modify(a, "B", 86);
-  printf("Expected:\nThe train name is the D train \n Your train, on a daily basis, has 90 delays\n Results\n");
-  modify(a, "D",90);
-  printf("Expected:The train name is the C train \n Your train, on a daily basis, has 100000 delays\n Results\n");
-  modify(a, "C", 100000);
+  printf("Expected:\nThe train name is the B train \nYour train, on a daily basis, has 86 delays\n Results:\n");
+  modify(a, s1, 86);
+  printf("Expected:\nThe train name is the D train \nYour train, on a daily basis, has 90 delays\n Results:\n");
+  modify(a, s2,90);
+  printf("Expected:\nThe train name is the C train \nYour train, on a daily basis, has 100000 delays\n Results:\n");
+  modify(a, s3, 100000);
+  
+  randomize();
+
   randomize();
   return 0;
 }
